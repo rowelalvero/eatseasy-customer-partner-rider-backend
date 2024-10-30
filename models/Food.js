043b59d9889eeef0cbca8c3e00b94283e1cb073b
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2024-10-30 11:04:08 +0800

    Added update food details.

--- models/Food.js
+++ models/Food.js
@@ -1,36 +1,11 @@
 const mongoose = require('mongoose');
 
-const additiveSchema = new mongoose.Schema({
-    id: { type: Number, required: true },
-    title: { type: String, required: true },
-    price: { type: Number, required: true },
-});
-
-const customAdditiveSchema = new mongoose.Schema({
-    id: { type: Number, required: true },
-    text: { type: String, required: true },
-    type: { type: String, required: true },
-    options: [{
-        optionName: { type: String, required: true },
-        price: { type: Number, required: true },
-    }],
-    linearScale: { type: Number },
-    minScale: { type: Number },
-    maxScale: { type: Number },
-    minScaleLabel: { type: String },
-    maxScaleLabel: { type: String },
-    required: { type: Boolean, default: false },
-    selectionType: { type: String },
-    selectionNumber: { type: Number },
-    customErrorMessage: { type: String },
-});
-
 const foodSchema = new mongoose.Schema({
     title: { type: String, required: true },
     time: { type: String, required: true },
-    foodTags: [{ type: String, required: true }],
+    foodTags: [{ type: String, required: true }], // Specify the type of items in the array
     category: { type: String, required: true },
-    foodType: [{ type: String, required: true }],
+    foodType: [{ type: String, required: true }], // Specify the type of items in the array
     code: { type: String, required: true },
     isAvailable: { type: Boolean, required: true, default: true },
     restaurant: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant" },
@@ -40,12 +15,11 @@ const foodSchema = new mongoose.Schema({
         max: 5,
         default: 3,
     },
-    ratingCount: { type: Number, default: 302 },
+    ratingCount: { type: Number, default: 302 }, // Change to Number
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    additives: [additiveSchema],
-    additiveQuestions: [customAdditiveSchema],
-    imageUrl: [{ type: String, required: true }],
+    additives: [{ type: String, required: true }], // Specify the type of items in the array
+    imageUrl: [{ type: String, required: true }], // Specify the type of items in the array
 });
 
 module.exports = mongoose.model('Food', foodSchema);
