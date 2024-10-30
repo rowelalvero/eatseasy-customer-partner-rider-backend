61dfa6de4f9924f5093390fc537530c19637f05e
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2024-10-30 11:23:26 +0800

    Added update food details.

--- models/Food.js
+++ models/Food.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const foodSchema = new mongoose.Schema({
     title: { type: String, required: true },
     time: { type: String, required: true },
-    foodTags: [{ type: String, required: true }], // Specify the type of items in the array
-    category: { type: String, required: true },
-    foodType: [{ type: String, required: true }], // Specify the type of items in the array
-    code: { type: String, required: true },
-    isAvailable: { type: Boolean, required: true, default: true },
+    foodTags: {type: Array, required: true},
+    category: {type: String, required: true},
+    foodType: {type: Array, required: true},
+    code: {type: String, required: true},
+    isAvailable: {type: Boolean , required: true, default: true},
     restaurant: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant" },
     rating: {
         type: Number,
@@ -15,11 +15,14 @@ const foodSchema = new mongoose.Schema({
         max: 5,
         default: 3,
     },
-    ratingCount: { type: Number, default: 302 }, // Change to Number
+    ratingCount: {type: String, default: "302"},
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    additives: [{ type: String, required: true }], // Specify the type of items in the array
-    imageUrl: [{ type: String, required: true }], // Specify the type of items in the array
+    additives: { type: Array, required: true },
+    customAdditives: { type: Array, required: true },
+    imageUrl: { type: Array, required: true },
 });
 
 module.exports = mongoose.model('Food', foodSchema);
+
+
