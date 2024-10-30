39582af5f5a58ca102c559648531751cdeb921d5
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2024-10-30 10:47:23 +0800

    Added update food details.

--- models/Food.js
+++ models/Food.js
@@ -1,13 +1,38 @@
 const mongoose = require('mongoose');
 
+const additiveSchema = new mongoose.Schema({
+    id: { type: Number, required: true },
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+});
+
+const customAdditiveSchema = new mongoose.Schema({
+    id: { type: Number, required: true },
+    text: { type: String, required: true },
+    type: { type: String, required: true },
+    options: [{
+        optionName: { type: String, required: true },
+        price: { type: Number, required: true },
+    }],
+    linearScale: { type: Number },
+    minScale: { type: Number },
+    maxScale: { type: Number },
+    minScaleLabel: { type: String },
+    maxScaleLabel: { type: String },
+    required: { type: Boolean, default: false },
+    selectionType: { type: String },
+    selectionNumber: { type: Number },
+    customErrorMessage: { type: String },
+});
+
 const foodSchema = new mongoose.Schema({
     title: { type: String, required: true },
     time: { type: String, required: true },
-    foodTags: {type: Array, required: true},
-    category: {type: String, required: true},
-    foodType: {type: Array, required: true},
-    code: {type: String, required: true},
-    isAvailable: {type: Boolean , required: true, default: true},
+    foodTags: [{ type: String, required: true }],
+    category: { type: String, required: true },
+    foodType: [{ type: String, required: true }],
+    code: { type: String, required: true },
+    isAvailable: { type: Boolean, required: true, default: true },
     restaurant: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant" },
     rating: {
         type: Number,
@@ -15,13 +40,12 @@ const foodSchema = new mongoose.Schema({
         max: 5,
         default: 3,
     },
-    ratingCount: {type: String, default: "302"},
+    ratingCount: { type: Number, default: 302 },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    additives: { type: Array, required: true },
-    imageUrl: { type: Array, required: true },
+    additives: [additiveSchema],
+    additiveQuestions: [customAdditiveSchema],
+    imageUrl: [{ type: String, required: true }],
 });
 
 module.exports = mongoose.model('Food', foodSchema);
-
-
