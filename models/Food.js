fe681c6c2c1405580db207c69926ff8641b9d5d2
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2024-10-30 14:49:16 +0800

    Added update food details.

--- models/Food.js
+++ models/Food.js
@@ -1,52 +1,25 @@
 const mongoose = require('mongoose');
 
-const additiveSchema = new mongoose.Schema({
-    id: { type: Number, required: true },
-    title: { type: String, required: true },
-    price: { type: String, required: true }
-});
-
-const optionsSchema = new mongoose.Schema({
-    optionName: { type: String, required: true },
-    price: { type: String, required: true }
-});
-
-const customAdditivesSchema = new mongoose.Schema({
-    id: { type: Number, required: true },
-    text: { type: String, required: true },
-    type: { type: String, required: false },
-    options: { type: [optionsSchema], required: false }, // Array of options
-    linearScale: { type: Number, required: false },
-    minScale: { type: Number, required: false },
-    maxScale: { type: Number, required: false },
-    minScaleLabel: { type: String, required: false },
-    maxScaleLabel: { type: String, required: false },
-    required: { type: Boolean, default: false },
-    selectionType: { type: String, required: false },
-    selectionNumber: { type: Number, required: false },
-    customErrorMessage: { type: String, required: false }
-});
-
 const foodSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    foodTags: { type: [String], required: true }, // Array of strings
-    foodType: { type: [String], required: true }, // Array of strings
-    code: { type: String, required: false },
-    isAvailable: { type: Boolean, required: false, default: true },
-    restaurant: { type: String, required: false },
-    rating: { type: Number, required: false },
-    ratingCount: { type: String, required: false },
+    time: { type: String, required: true },
+    foodTags: {type: Array, required: true},
+    category: {type: String, required: true},
+    foodType: {type: Array, required: true},
+    code: {type: String, required: true},
+    isAvailable: {type: Boolean , required: true, default: true},
+    restaurant: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant" },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        default: 3,
+    },
+    ratingCount: {type: String, default: "302"},
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    additives: { type: [additiveSchema], required: true }, // Array of Additive subdocuments
-    imageUrl: { type: [String], required: true }, // Array of strings
-    v: { type: Number, required: false },
-    category: { type: String, required: false },
-    time: { type: String, required: false },
-    customAdditives: { type: [customAdditivesSchema], required: true } // Array of CustomAdditives subdocuments
+    additives: { type: Array, required: true },
+    imageUrl: { type: Array, required: true },
 });
 
-// Create a Food model
-const Food = mongoose.model('Food', foodSchema);
-
-module.exports = Food;
+module.exports = mongoose.model('Food', foodSchema);
