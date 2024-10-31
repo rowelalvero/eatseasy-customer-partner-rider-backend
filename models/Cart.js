ece5fb2f6c49460df8e6e83c503d6b80928b0eb4
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2024-10-31 21:32:40 +0800

    Added update food details.

--- models/Cart.js
+++ models/Cart.js
@@ -3,14 +3,13 @@ const mongoose = require('mongoose');
 const cartSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     productId: { type: mongoose.Schema.Types.ObjectId, ref: "Food" },
-    additives: {type: [] },
-    instructions: {type: String, default: ''},
-    totalPrice: {type: Number , required: true},
-    quantity: {type: Number , required: true},
-    prepTime: {type: String , default: ''},
+    additives: { type: Array },
+    instructions: { type: String, default: '' },
+    totalPrice: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    prepTime: { type: String, default: '' },
     restaurant: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant" },
-    customAdditives: {type: [] },
+    customAdditives: { type: Map, of: mongoose.Schema.Types.Mixed }, // Updated to allow various data types
 });
 
 module.exports = mongoose.model('Cart', cartSchema);
-
