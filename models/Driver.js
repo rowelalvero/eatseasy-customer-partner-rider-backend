cacd046f95cfb2553909171b1bbf88d8c929a6b2
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2024-11-02 02:04:28 +0800

    Added update food details.

--- models/Driver.js
+++ models/Driver.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
+
+const walletTransactionSchema = new mongoose.Schema({
+    amount: { type: Number, required: true },
+    paymentMethod: { type: String, required: true },
+    transactionDate: { type: Date, default: Date.now }, // Optional field to log transaction date
+});
+
 const driverSchema = new mongoose.Schema({
     driver: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    wallet: { type: mongoose.Schema.Types.ObjectId, ref: "Wallet" },
     vehicleType: { type: String, required: true, enum: ['Bike', 'Car', 'Scooter', 'Drone'] },
     phone: { type: String, required: true, default: '1234567890' },
     vehicleNumber: { type: String, required: true },
@@ -9,6 +15,8 @@ const driverSchema = new mongoose.Schema({
         latitude: { type: Number, required: true },
         longitude: { type: Number, required: true },
     },
+    walletTransactions: [walletTransactionSchema], // Embeds the walletTransactionSchema for transactions
+    walletBalance: { type: Number, default: 0 }, // New field for driver’s wallet balance
     isAvailable: { type: Boolean, default: true },
     verification: { type: String, default: "Pending", enum: ["Pending", "Verified", "Rejected"] },
     verificationMessage: { type: String, default: "Please allow up to 24 hours for your verification to be processed. You will receive a notification once your verification is complete." },
@@ -16,6 +24,7 @@ const driverSchema = new mongoose.Schema({
     totalDeliveries: { type: Number, default: 0 },
     totalEarnings: { type: Number, default: 0.0 },
     profileImage: { type: String, default: "https://d326fntlu7tb1e.cloudfront.net/uploads/cf503f8d-3318-4306-9e2c-a53efb7d585b-avatar.png" },
-    isActive: { type: Boolean, default: false } // To track if the driver is currently active on the platform
+    isActive: { type: Boolean, default: false }
 });
+
 module.exports = mongoose.model('Driver', driverSchema);
--- routes/wallet.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const walletController = require("../controllers/walletController");
-const { verifyTokenAndAuthorization } = require("../middlewares/verifyToken");
-
-router.get("/", verifyTokenAndAuthorization, walletController.getWalletDetails);
-router.post("/topup", verifyTokenAndAuthorization, walletController.topUpWallet);
-router.post("/withdraw", verifyTokenAndAuthorization, walletController.withdrawFromWallet);
-
-module.exports = router;
