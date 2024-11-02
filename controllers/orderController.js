70fb749217c9aa421862f7d615708bbc02a0e182
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2024-11-02 16:50:11 +0800

    Added update food details.

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -295,7 +295,7 @@ module.exports = {
         //paymentStatus: "Completed",
       })
         .select(
-          "userId deliveryAddress deliveryOption deliveryDate orderItems orderTotal deliveryFee restaurantId orderStatus restaurantCoords recipientCoords orderDate"
+          "userId deliveryAddress deliveryOption paymentStatus deliveryDate orderItems orderTotal deliveryFee restaurantId orderStatus restaurantCoords recipientCoords orderDate"
         )
         .populate({
           path: "userId",
