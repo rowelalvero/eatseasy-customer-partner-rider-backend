620d85eec26afb03b7b466f114fc3ebbd51f5493
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2024-11-08 01:28:00 +0800

    Added getRandomFoods

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -175,12 +175,10 @@ module.exports = {
             let randomFoodList = [];
             
             // If no code provided in params or no Foods match the provided code
-            if (!randomFoodList.length) {
-                randomFoodList = await Food.aggregate([
-                    { $sample: { size: 5 } },
-                    { $project: {  __v: 0 } }
-                ]);
-            }
+            randomFoodList = await Food.aggregate([
+                                { $sample: { size: 5 } },
+                                { $project: {  __v: 0 } }
+                            ]);
     
             // Respond with the results
             if (randomFoodList.length) {
