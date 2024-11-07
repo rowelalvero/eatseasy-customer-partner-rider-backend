09780a40d7c8e91559de3972d1cf02bb315e8a5c
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2024-11-08 01:51:25 +0800

    Added getRandomFoods

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -57,19 +57,34 @@ module.exports = {
             const sampleSize = parseInt(req.query.size) || 5; // Use a default size of 5 if no size is specified
 
             try {
-                const randomFoods = await Food.aggregate([
-                    { $sample: { size: sampleSize } }, // Fetch a random selection of foods
-                    { $project: { __v: 0 } } // Exclude the __v field
-                ]);
+                        let randomFoodList = [];
 
-                if (randomFoods.length === 0) {
-                    return res.status(404).json({ status: false, message: 'No foods found' });
-                }
+                        // Check if code is provided in the params
+                        if (req.params.code) {
+                            randomFoodList = await Food.aggregate([
+                                { $match: { code: req.params.code } },
+                                { $sample: { size: 3 } },
+                                { $project: {  __v: 0 } }
+                            ]);
+                        }
 
-                res.status(200).json(randomFoods);
-            } catch (error) {
-                res.status(500).json({ status: false, message: error.message });
-            }
+                        // If no code provided in params or no Foods match the provided code
+                        if (!randomFoodList.length) {
+                            randomFoodList = await Food.aggregate([
+                                { $sample: { size: 5 } },
+                                { $project: {  __v: 0 } }
+                            ]);
+                        }
+
+                        // Respond with the results
+                        if (randomFoodList.length) {
+                            res.status(200).json(randomFoodList);
+                        } else {
+                            res.status(404).json({status: false, message: 'No Foods found' });
+                        }
+                    } catch (error) {
+                        res.status(500).json(error);
+                    }
         },
 
 
