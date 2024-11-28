002b69f63a5eefde438923442f3db70343239786
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2024-11-28 16:11:13 +0800

    Checkpoint

--- routes/constant.js
+++ routes/constant.js
@@ -2,12 +2,8 @@
 
 const router = require('express').Router();
 const constantController = require('../controllers/constantController');
-const {verifyAdmin} = require('../middleware/verifyToken');
 
 // GET: Retrieve the commissionRate and driverBaseRate
 router.get('/', constantController.getConstants);
-
-// PUT: Update the commissionRate and driverBaseRate
-router.put('/', verifyAdmin, constantController.updateConstants);
 
 module.exports = router;
